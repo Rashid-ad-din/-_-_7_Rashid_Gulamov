0acfff89064517bf022ef4188f0ad3a439f344dd
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-10-01 12:24:27 +0600

    Завершен Этап 1 контрольной работы. Создана модель записи, зарегистрирована в админке, добавлено 5 записей (1 заблокированая), произведены соответствующие миграции, произведен дамп в фикстуру. Добавлены некоторые стили в css.

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Note
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'email', 'created_at', 'status')
+    list_filter = ('id', 'author', 'status')
+    search_fields = ('author', 'status')
+    fields = ('author', 'email', 'text', 'created_at', 'changed_at', 'status')
+    readonly_fields = ('id', 'created_at', 'changed_at')
+
+
+admin.site.register(Note, NoteAdmin)
--- app/webapp/models.py
+++ app/webapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import TextChoices
+
 
 # Create your models here.
+
+class StatusChoices(TextChoices):
+    ACTIVE = 'active', 'Активна'
+    BLOCKED = 'blocked', 'Заблокировано'
+
+
+class Note(models.Model):
+    author = models.CharField(max_length=150, null=False, blank=False, verbose_name='Автор')
+    email = models.EmailField(max_length=254, null=False, blank=False, verbose_name='Почта')
+    text = models.TextField(max_length=2500, null=False, blank=False, verbose_name='Текст')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    status = models.CharField(choices=StatusChoices.choices, max_length=100,
+                              default=StatusChoices.ACTIVE, verbose_name='Статус')
+
+    def __str__(self):
+        return f'Автор: {self.author}, Статус: {self.get_status_display()}, Дата создания: {self.created_at}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+
